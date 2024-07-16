62b78d829165453032f6d44db74a46cbb714339f
rmichaelis27 <rmichaelis27@gmail.com>
2024-07-16 09:34:40 -0400

    Update and rename make-pr-here.sql to Challenge_2_PR.sql
    Included distinct count of states by order joining on customer ID

--- second-challenge/Challenge_2_PR.sql
+++ second-challenge/Challenge_2_PR.sql
@@ -6,7 +6,9 @@ create view product_perf as
         , p.Price as Product_Price
         , sum(o.Quantity) as Total_Product_Sold
         , count(o.Order_Id) as Total_Product_Orders
+        , distinct count(c.State) as States_Per_Order
 
 from products p
-join orders o
-    on o.product_id = p.product_id
+join orders o, customers c
+    on o.product_id = p.product_id,
+    on c.cust_id = p.cust_id
